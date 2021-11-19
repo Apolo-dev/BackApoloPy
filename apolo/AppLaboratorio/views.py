666037333fb163c9dc13fd9984ded23adcf162e9
Eric Carvajal <carvajal.cortes.e19@gmail.com>
2021-11-19 12:30:23 -0300

    V9 Arregle el dram con la migraciones :)

--- apolo/AppLaboratorio/views.py
+++ apolo/AppLaboratorio/views.py
@@ -10,7 +10,7 @@ from math import sqrt
 # Create your views here.
 
 class SaveLab(viewsets.ModelViewSet):
-    queryset = LabGiratorio.objects.filter(id = 5)
+    queryset = LabGiratorio.objects.all()
     serializer_class = LabGiratorioSerializer
 
 class saveAbertura(viewsets.ModelViewSet):
